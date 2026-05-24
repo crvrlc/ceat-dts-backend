01caf0786abce09c3ed557d8f1efd17bf0beebfe
crvrlc <llcervera@up.edu.ph>
2026-05-25 02:37:31 +0800

    retry resend

--- services/emailService.js
+++ services/emailService.js
@@ -1,31 +1,5 @@
-const nodemailer = require('nodemailer');
-const { google } = require('googleapis');
-
-const createTransporter = async () => {
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.GMAIL_CLIENT_ID,
-    process.env.GMAIL_CLIENT_SECRET,
-    'https://developers.google.com/oauthplayground'
-  );
-
-  oauth2Client.setCredentials({
-    refresh_token: process.env.GMAIL_REFRESH_TOKEN,
-  });
-
-  const accessToken = await oauth2Client.getAccessToken();
-
-  return nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.EMAIL_USER,
-      clientId: process.env.GMAIL_CLIENT_ID,
-      clientSecret: process.env.GMAIL_CLIENT_SECRET,
-      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
-      accessToken: accessToken.token,
-    },
-  });
-};
+const { Resend } = require('resend');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const STATUS_LABELS = {
   submitted:     'Submitted',
@@ -154,14 +128,24 @@ const sendDocumentUpdateEmail = async ({ studentName, studentEmail, trackingCode
     </div>
   `.trim();
 
-  const transporter = await createTransporter();
+  // await resend.emails.send({
+  //   from: 'CEAT OCS Document Tracking System <onboarding@resend.dev>',
+  //   to: studentEmail,
+  //   subject: `Document Update [${trackingCode}] - ${statusLabel}`,
+  //   html,
+  // });
 
-  await transporter.sendMail({
-    from: `"CEAT OCS Document Tracking System" <${process.env.EMAIL_USER}>`,
-    to: studentEmail,
-    subject: `Document Update [${trackingCode}] - ${statusLabel}`,
-    html,
-  });
+  try {
+    const result = await resend.emails.send({
+      from: 'CEAT OCS Document Tracking System <onboarding@resend.dev>',
+      to: studentEmail,
+      subject: `Document Update [${trackingCode}] - ${statusLabel}`,
+      html,
+    });
+    console.log('Email result:', JSON.stringify(result));
+  } catch (err) {
+    console.error('Email error:', err.message);
+  }
 };
 
 module.exports = { sendDocumentUpdateEmail };
